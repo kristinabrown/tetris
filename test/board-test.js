cba544d4c24ca5abe14cd83d059775d479e15511
andrew carmer <andrewcarmer@andrews-MacBook-Pro.local>
2015-07-21 15:58:55 -0600

    tdd for blocks and board

--- test/board-test.js
+++ test/board-test.js
@@ -1,8 +1,21 @@
 const chai = require('chai');
 const assert = chai.assert;
+const board = require('../lib/board/index');
 
 describe('board', function () {
-  it('should work', function () {
-    assert(true);
+  it('should be an array', function () {
+    assert(Array.isArray(board));
+  });
+
+  it('should be an array of arrays', function () {
+    assert(Array.isArray(board[0]));
+  });
+
+  it('should be an array with twenty arrays', function () {
+    assert.equal(board.length, 20);
+  });
+
+  it('should be an array with twenty arrays and 10 0s in each array', function () {
+    assert.equal(board[0].length, 10);
   });
 });
--- test/index.js
+++ test/index.js
@@ -1,8 +1,2 @@
-const chai = require('chai');
-const assert = chai.assert;
-
-describe('my test suite', function () {
-  it('should work', function () {
-    assert(true);
-  });
-});
+require('./block-test.js');
+require('./board-test.js');
