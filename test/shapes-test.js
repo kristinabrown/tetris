97caf143a4c7b8726888dc0a77e654f87f237ea6
Andrew Carmer <carmerandrew@gmail.com>
2015-07-26 09:23:11 -0600

    cleanup all commented out code. Restructured for readability and comprehension

--- test/shapes-test.js
+++ test/shapes-test.js
@@ -8,39 +8,41 @@ const board = require('../lib/board').board;
 
 describe('shapes', function () {
   it('i is an array of blocks', function () {
-    assert(Array.isArray(i[0]));
+    assert(Array.isArray(i));
   });
 
   var pieces = [];
 
   it('it should be able to move down', function(){
     var testShape = new Shape();
+
+    var result = testShape.blocks[0].y + 1
     testShape.blocks.forEach(function(block){pieces.push(block)});
 
     testShape.down(board);
 
-    assert.equal(testShape.blocks[0].y, 1)
+    assert.equal(testShape.blocks[0].y, result)
   });
 
   it('it should should be able to move right', function() {
     var testShape = new Shape();
+    var result = testShape.blocks[0].x + 1
+
     testShape.blocks.forEach(function(block){pieces.push(block)});
     testShape.right(board, pieces);
 
-    assert.equal(testShape.blocks[0].x, 5)
+    assert.equal(testShape.blocks[0].x, result)
   });
 
-  it('it should should be able to move right', function() {
+  it('it should should be able to move left', function() {
     var testShape = new Shape();
-    console.log(testShape.blocks[0].x)
+    var result = testShape.blocks[0].x - 1
+
     testShape.blocks.forEach(function(block){pieces.push(block)});
     testShape.left(board, pieces);
 
-    assert.equal(testShape.blocks[0].x, 4)
+    assert.equal(testShape.blocks[0].x, result)
   });
-
-
-
 
 
 });
