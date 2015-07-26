34f93d054ad5c323a69915818917cd893b0874da
Andrew Carmer <carmerandrew@gmail.com>
2015-07-25 19:42:03 -0600

    block down

--- test/game-test.js
+++ test/game-test.js
@@ -26,23 +26,28 @@ var board = [ [0,0,0,0,0,0,0,0,0,0],
               [1,1,1,1,1,1,1,0,1,1]
             ]
 
+  var pieces = []
+
   it('clear a row when a block sets', function () {
     var b = new Block(7, 18);
-    b.down(board);
-    b.down(board);
+    pieces.push(b);
+    b.down(board, pieces);
+    b.down(board, pieces);
 
     assert.deepEqual([0,0,0,0,0,0,0,0,0,0], board[19]);
   });
 
   it('changes grid value from 0 to 1', function () {
     var b = new Block(3, 3);
-    b.down(board);
+    pieces.push(b);
+    b.down(board, pieces);
     assert.equal(1, board[4][3]);
   });
 
   it('changes grid value from 1 to 0', function () {
     var b = new Block(3, 3);
-    b.down(board);
+    pieces.push(b);
+    b.down(board, pieces);
     assert.equal(0, board[0][3]);
   });
 });
--- test/shapes-test.js
+++ test/shapes-test.js
@@ -1,8 +1,9 @@
 const chai = require('chai');
 const assert = chai.assert;
 const i = require('../lib/shapes').i;
-const Shape = require('../lib/shapes').shape;
+const Shape = require('../lib/shapes').Shape;
 const Block = require('../lib/block').Block;
+const board = require('../lib/board').board;
 
 
 describe('shapes', function () {
@@ -11,11 +12,10 @@ describe('shapes', function () {
   });
 
   it('it should be able to move down', function(){
-    var testShape = new Shape()
+    var testShape = new Shape();
 
-    var result = testShape.down();
-
-    assert.equal(testShape.blocks[0], 0)
+    var result = testShape.down(board);
+    assert.equal(result[3].y, 1)
   });
 
 
